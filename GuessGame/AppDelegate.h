772dc0feb15a8242f34eecf2391b54427e6f7d70
gyw520gyw <893728221@qq.com>
2016-09-22 23:43:42 +0800

    first commit

---
+++ GuessGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuessGame
+//
+//  Created by gyw on 16/9/22.
+//  Copyright © 2016年 gyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuessGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuessGame
+//
+//  Created by gyw on 16/9/22.
+//  Copyright © 2016年 gyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
