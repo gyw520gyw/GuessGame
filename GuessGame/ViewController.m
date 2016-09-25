23731fb674ac99b982a766e0161ff696c572af3b
gyw520gyw <893728221@qq.com>
2016-09-25 22:57:17 +0800

    使用懒加载创建蒙版

--- GuessGame/ViewController.m
+++ GuessGame/ViewController.m
@@ -12,7 +12,10 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *iconButton;
 
+@property (nonatomic, strong) UIButton *conver;
+
 @end
+
 
 @implementation ViewController
 
@@ -20,15 +23,28 @@
     [super viewDidLoad];
 }
 
+//使用懒加载
+- (UIButton *)conver {
+    
+    if(_conver == nil) {
+    
+        _conver = [[UIButton alloc]initWithFrame:self.view.bounds];
+        _conver.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+        [self.view addSubview:_conver];
+        _conver.alpha = 0.0;
+        
+        [_conver addTarget:self action:@selector(smallImg:) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+    
+    return _conver;
+    
+}
+
 - (IBAction)bigImg:(UIButton *)button {
     
     //1.添加蒙版
-    UIButton *conver = [[UIButton alloc]initWithFrame:self.view.bounds];
-    conver.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.5];
-    [self.view addSubview:conver];
-    conver.alpha = 0.0;
-    
-    [conver addTarget:self action:@selector(smallImg:) forControlEvents:UIControlEventTouchUpInside];
+    [self conver];
     
     //2.图片置顶
     [self.view bringSubviewToFront:self.iconButton];
@@ -40,7 +56,7 @@
     
     [UIView animateWithDuration:1.0 animations:^{
         self.iconButton.frame = CGRectMake(0, y, width, width);
-        conver.alpha = 1.0;
+        self.conver.alpha = 1.0;
     }];
 }
 
@@ -54,7 +70,7 @@
         conver.alpha = 0.0;
     } completion:^(BOOL finished) {
         //2.删除蒙版
-        [conver removeFromSuperview];
+//        [conver removeFromSuperview];
     }];
     
     
